0082ca43410deedfcfa038de24afeb46e97ecd30
jugosack <jugoslav_ackoski@yahoo.com>
2023-06-01 09:57:24 +0200

    Fixing rubocop erros

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
- # Include default devise modules. Others available are:
- # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
   has_many :foods
   has_many :recipes
-  
+
   ############# THIS IS COMMENT FOR CHECKING AUTHENTICATION WITHOUT AUTHORIZATION ####################
   # validates :name, presence: true, length: { in: 1..50 }
 end
