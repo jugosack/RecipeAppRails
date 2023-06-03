b23fe1d3999a776e8c8f453e24b9df040296bdda
jugosack <jugoslav_ackoski@yahoo.com>
2023-06-03 06:29:41 +0200

    Add changes in user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,22 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   describe 'associations' do
-#     it { should have_many(:foods) }
-#     it { should have_many(:recipes) }
-#   end
+RSpec.describe User, type: :model do
+    subject { User.new(name: 'John') }
+  
+    before { subject.save }
+  
+    it 'validates presence of name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+    it 'check the name is not blank' do
+              subject.name = nil
+              expect(subject).to_not be_valid
+            end
 
-#   describe 'validations' do
-#     it { should validate_presence_of(:name) }
-#     it { should validate_length_of(:name).is_at_least(1).is_at_most(50) }
-#   end
-# end
+            it 'check the email is not blank' do
+                subject.email = nil
+                expect(subject).to_not be_valid
+                end
+    
+end
